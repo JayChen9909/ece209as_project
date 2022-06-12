91bc5300bf059bd0b787a9ea5467016e73953084
halfgodxzc <82023708+halfgodxzc@users.noreply.github.com>
2022-06-13 06:08:28 +0800

    upload backbones

--- software/mmediting/mmedit/models/__init__.py
+++ software/mmediting/mmedit/models/__init__.py
@@ -1,27 +1,33 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .backbones import *  # noqa: F401, F403
-from .base import BaseModel
-from .builder import (build, build_backbone, build_component, build_loss,
-                      build_model)
-from .common import *  # noqa: F401, F403
-from .components import *  # noqa: F401, F403
-from .extractors import LTE, FeedbackHourglass
-from .inpaintors import (AOTInpaintor, DeepFillv1Inpaintor, GLInpaintor,
-                         OneStageInpaintor, PConvInpaintor, TwoStageInpaintor)
-from .losses import *  # noqa: F401, F403
-from .mattors import DIM, GCA, BaseMattor, IndexNet
-from .registry import BACKBONES, COMPONENTS, LOSSES, MODELS
-from .restorers import ESRGAN, SRGAN, BasicRestorer
-from .synthesizers import CycleGAN, Pix2Pix
-from .transformers import SearchTransformer
-from .video_interpolators import CAIN, BasicInterpolator
+from .encoder_decoders import (VGG16, ContextualAttentionNeck, DeepFillDecoder,
+                               DeepFillEncoder, DeepFillEncoderDecoder,
+                               DepthwiseIndexBlock, FBADecoder,
+                               FBAResnetDilated, GLDecoder, GLDilationNeck,
+                               GLEncoder, GLEncoderDecoder, HolisticIndexBlock,
+                               IndexedUpsample, IndexNetDecoder,
+                               IndexNetEncoder, PConvDecoder, PConvEncoder,
+                               PConvEncoderDecoder, PlainDecoder,
+                               ResGCADecoder, ResGCAEncoder, ResNetDec,
+                               ResNetEnc, ResShortcutDec, ResShortcutEnc,
+                               SimpleEncoderDecoder)
+from .generation_backbones import ResnetGenerator, UnetGenerator
+from .sr_backbones import (EDSR, LIIFEDSR, LIIFRDN, RDN, SRCNN, BasicVSRNet,
+                           BasicVSRPlusPlus, DICNet, EDVRNet, GLEANStyleGANv2,
+                           IconVSR, MSRResNet, RealBasicVSRNet, RRDBNet,
+                           TDANNet, TOFlow, TTSRNet)
+from .vfi_backbones import CAINNet, FLAVRNet, TOFlowVFINet
 
 __all__ = [
-    'AOTInpaintor', 'BaseModel', 'BasicRestorer', 'OneStageInpaintor', 'build',
-    'build_backbone', 'build_component', 'build_loss', 'build_model',
-    'BACKBONES', 'COMPONENTS', 'LOSSES', 'BaseMattor', 'DIM', 'MODELS',
-    'GLInpaintor', 'PConvInpaintor', 'SRGAN', 'ESRGAN', 'GCA',
-    'TwoStageInpaintor', 'IndexNet', 'DeepFillv1Inpaintor', 'Pix2Pix',
-    'CycleGAN', 'SearchTransformer', 'LTE', 'FeedbackHourglass',
-    'BasicInterpolator', 'CAIN'
+    'MSRResNet', 'VGG16', 'PlainDecoder', 'SimpleEncoderDecoder',
+    'GLEncoderDecoder', 'GLEncoder', 'GLDecoder', 'GLDilationNeck',
+    'PConvEncoderDecoder', 'PConvEncoder', 'PConvDecoder', 'ResNetEnc',
+    'ResNetDec', 'ResShortcutEnc', 'ResShortcutDec', 'RRDBNet',
+    'DeepFillEncoder', 'HolisticIndexBlock', 'DepthwiseIndexBlock',
+    'ContextualAttentionNeck', 'DeepFillDecoder', 'EDSR', 'RDN', 'DICNet',
+    'DeepFillEncoderDecoder', 'EDVRNet', 'IndexedUpsample', 'IndexNetEncoder',
+    'IndexNetDecoder', 'TOFlow', 'ResGCAEncoder', 'ResGCADecoder', 'SRCNN',
+    'UnetGenerator', 'ResnetGenerator', 'FBAResnetDilated', 'FBADecoder',
+    'BasicVSRNet', 'IconVSR', 'TTSRNet', 'GLEANStyleGANv2', 'TDANNet',
+    'LIIFEDSR', 'LIIFRDN', 'BasicVSRPlusPlus', 'RealBasicVSRNet', 'CAINNet',
+    'TOFlowVFINet', 'FLAVRNet'
 ]
